2f27976505a9d3af8b3d7277154bd8ed6c785df5
Aaron Oriasel <aaronoriasel321@gmail.com>
2025-06-14 22:44:11 +0800

    Save local changes to Userpage.jsx

--- src/Userpage.jsx
+++ src/Userpage.jsx
@@ -7,11 +7,7 @@ function user() {
         <>
           <Nav/>
           <Hero/>
-<<<<<<< HEAD
-          <ProductSection />
-=======
-            <section id="product" className="py-16"> </section>
->>>>>>> aea4aaa358e35bf88ed4c9ebe251342560042de0
+        <ProductSection />
             <section id="transactions" className="py-16"></section>
             <section id="about-us" className="py-16"></section>
             <section id="contact" className="py-16"></section>

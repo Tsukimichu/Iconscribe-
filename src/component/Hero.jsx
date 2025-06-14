83dd7f11fec0ce50f6a49ef80a584aca406d9622
Tsukimichu <valenzuela.joshua@mscmarinduque.edu.ph>
2025-06-14 23:14:26 +0800

    fix spacing

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,11 +7,11 @@ function Hero(){
         className="relative h-[500px] bg-cover bg-center flex items-center px-6 sm:px-10 lg:px-20 mt-0"
         style={{ backgroundImage: `url(${bg})` }}
         >
-      {/* Optional dark overlay for readability */}
+
       <div className="absolute inset-0 bg-black/50"></div>
 
-      {/* Text Content */}
       <div className="relative z-10 text-white max-w-xl">
+        
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold leading-snug">
           Fast.<br />
           Reliable.<br />

a6457b67438ca0e7d5284d8c75b0d8e421241de2
Tsukimichu <valenzuela.joshua@mscmarinduque.edu.ph>
2025-06-14 22:40:25 +0800

    fixed error

--- src/Userpage.jsx
+++ src/Userpage.jsx
@@ -1,17 +1,14 @@
 import Nav from "./component/navigation"
 import Hero from "./component/Hero"
-import ProductSection from "./component/ProductSection";
+import ProductSection from "./component/ProductSection"
 
 function user() {
     return(
         <>
           <Nav/>
           <Hero/>
-<<<<<<< HEAD
-          <ProductSection />
-=======
+          <ProductSection/>          
             <section id="product" className="py-16"> </section>
->>>>>>> aea4aaa358e35bf88ed4c9ebe251342560042de0
             <section id="transactions" className="py-16"></section>
             <section id="about-us" className="py-16"></section>
             <section id="contact" className="py-16"></section>
